7d18479b13b2a92443f0381f5104609f1a37303e
jashrashne <jash.rashne@gmail.com>
2024-11-16 22:54:00 +0530

    express app is alive

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,31 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import express from 'express';
+// import cors from 'cors';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = express()
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+//Routes
+app.get('/hello-world' , (req, res) => {
+    return res.status(200).send("Hello World");
+})
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+//Create
+//post
+
+
+//Read
+//get
+
+//Update
+//put
+
+//Delete
+//delete
+
+
+
+
+// Export the Express app as a Firebase Function
+export const api = functions.https.onRequest(app);
